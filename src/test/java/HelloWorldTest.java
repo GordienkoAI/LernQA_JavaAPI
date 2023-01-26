b14cdeb0839a66c48d752e45cc1bc599afca2367
andreygordienko <gordienko.ani@open.ru>
2023-01-26 17:45:35 +0300

    Hello from

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,11 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class HelloWorldTest {
 
     @Test
     public void testHelloWorld(){
-        System.out.println("Hello world!");
+        System.out.println("Hello from Gordienko Andrey");
     }
 }
