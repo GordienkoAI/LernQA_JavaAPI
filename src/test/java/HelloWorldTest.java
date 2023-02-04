d85e277ccd7f73fc8e45cdc389212d013d4440ad
andreygordienko <gordienko.ani@open.ru>
2023-02-04 08:09:21 +0300

    Ex5: Парсинг JSON

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,15 +1,54 @@
+
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld(){
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
+    public void testRestAssured(){
+
+        Response firstHome = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
                 .andReturn();
 
-        response.prettyPrint();
+        firstHome.print();
+
+/*
+
+        Map<String, String> data = new HashMap<>();
+        data.put("login", "secret_login");
+        data.put("password", "secret_pass");
+
+        Response responseForGet = RestAssured
+                .given()
+                .body(data)
+                .when()
+                .post("https://playground.learnqa.ru/api/get_auth_cookie")
+                .andReturn();
+
+        String responceCookies = responseForGet.getCookie("auth_cookie");
+
+        Map<String,String> cookies = new HashMap<>();
+        if (responceCookies != null) {
+            cookies.put("auth_cookie",responceCookies );
+        }
+
+        Response responceForCheck = RestAssured
+                .given()
+                .body(data)
+                .cookies(cookies)
+                .when()
+                .post("https://playground.learnqa.ru/api/check_auth_cookie")
+                .andReturn();
+
+         responceForCheck.prettyPrint();
+
+
+ */
     }
+
 }
